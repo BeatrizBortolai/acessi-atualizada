f8e8935c4ac7f57cc45b26a55732194bb4aaad98
BeatrizBortolai <rm560186@fiap.com.br>
2025-05-06 09:33:16 -0300

    feat:update

---
+++ src/main/java/org/acme/resources/GreetingResource.java
@@ -0,0 +1,11 @@
+package org.acme.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/")
+public class GreetingResource {
+
+}
